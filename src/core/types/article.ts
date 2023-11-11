81b6b75e15a730a7960662fefe333ae6b08d0a4f
YggdrasillBR <ferreirac.lucas1@gmail.com>
2023-11-11 15:03:02 -0300

    core I

---
+++ src/core/types/article.ts
@@ -0,0 +1,19 @@
+import { Profile } from './profile'
+
+
+
+
+
+export type article = {
+    slug: string
+    title: string
+    description: string
+    body: string
+    tags: string
+    createdat: string
+    updatedat: string
+    favorited: boolean
+    favoritedNumber: number
+    author: Profile
+}
+
---
+++ src/core/types/user.ts
@@ -0,0 +1,7 @@
+export type user = {
+    email: string
+    token: string
+    username: string
+    bio: string
+    image : string
+}
